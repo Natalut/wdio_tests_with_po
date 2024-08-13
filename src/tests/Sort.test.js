d49b04135e8fb55345371976712cb794e782a934
Natalut <natalut9@gmail.com  git config --global user.email you@example.com  git config --global user.name Your>
2024-08-13 17:43:10 +0300

    Test_1_Sort

---
+++ src/tests/Sort.test.js
@@ -0,0 +1,46 @@
+const { app } = require('../pages/Application'); 
+
+
+describe('Inventory Sorting', () => {
+    before(async () => {
+        await app.login.navigate();
+        await app.login.performLogin('standard_user', 'secret_sauce');
+    });
+
+    it('should sort items by name (A to Z)', async () => {
+        await app.inventory.navigate();
+
+        const inventoryItems = await app.inventory.inventoryItems;
+        if (!inventoryItems.length) {
+            throw new Error('No inventory items found on the page');
+        }
+
+        const initialNames = [];
+        for (const item of inventoryItems) {
+            const itemName = await item.$('.inventory_item_name').getText();
+            initialNames.push(itemName);
+        }
+
+        console.log('Initial names:', initialNames);
+
+        const sortDropdown = await $('select.product_sort_container');
+        if (!await sortDropdown.isDisplayed()) {
+            throw new Error('No inventory items found on the page');
+        }
+        await sortDropdown.selectByAttribute('value', 'az');
+
+        const sortedNames = [];
+        for (const item of inventoryItems) {
+            const itemName = await item.$('.inventory_item_name').getText();
+            sortedNames.push(itemName);
+        }
+
+        console.log('Sorted names:', sortedNames);
+
+        const expectedSortedNames = [...initialNames].sort();
+
+        console.log('Expected Sorted Names:', expectedSortedNames);
+    }
+)
+}
+)
